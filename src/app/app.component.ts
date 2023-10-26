4fa42f353a16e48ca7b90fc031a5c83784abec6f
saurav raj <saurved785@gmail.com>
2023-10-26 13:47:56 +0530

    preparing frontend templated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'automate-esign-document-frontend';
+
+  uploadFile = () => {
+    console.log('I am in uplaod');
+    return;
+  };
+
+  onFileSelected = (event: any) => {
+    console.log('event', event);
+    return;
+  };
 }
